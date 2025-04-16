45b5062f4c807c94566020fa04f4cd2eea15a68f
ryanelouarrat <extraviruschime@gmail.com>
2025-04-16 19:14:49 +0100

    front end v1

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  # Temporary method to handle current user until authentication is fully implemented
+  def current_user
+    # Currently using static user ID = 1
+    Utilisateur.find_by(id: 1)
+  rescue
+    nil
+  end
+  
+  # Helper to check if user is logged in
+  def logged_in?
+    !current_user.nil?
+  end
 end
